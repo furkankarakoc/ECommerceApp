61ad821ebfa40a638a0349e356dee595b28aaade
Furkan Karakoc <44609894+furkankarakoc@users.noreply.github.com>
2025-07-27 15:18:08 +0300

    Initial Commit

---
+++ ECommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ECommerce
+//
+//  Created by furkankarakoc on 27.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ECommerceTests/ECommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceTests.swift
+//  ECommerceTests
+//
+//  Created by furkankarakoc on 27.07.2025.
+//
+
+import Testing
+@testable import ECommerce
+
+struct ECommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
